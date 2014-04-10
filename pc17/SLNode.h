6601ff46563e28b2bd3d186c4af79a7ad0f92e65
OpenShift guest <lovodi@hotmail.com>
2014-04-10 01:01:11 -0400

    updated 4/9/14 10:00

--- pc17/SLNode.h
+++ pc17/SLNode.h
@@ -6,14 +6,21 @@ using namespace std;
 class SLNode
 {
     public:
+        //sets empty node, NULL node and 0 contents;
         SLNode();
+        //sets nodes to contents
         SLNode(int newContents);
+        //node destructor
         virtual ~SLNode();
         
+        //retrieves contents from nodes
         int getContents() const;
+        //retrieves contents from the next node over
         SLNode* getNextNode() const;
         
+        //sets next node in the list
         void setNextNode(SLNode* newNextNode);
+        //sets contents for the new contents
         void setContents(int newContents);
         
     private:
