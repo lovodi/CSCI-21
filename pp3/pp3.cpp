7275299edcc0fb82966256fea8b4ec9118551dd8
OpenShift guest <lovodi@hotmail.com>
2014-04-25 03:40:53 -0400

    still need to fix 'B'

--- pp3/pp3.cpp
+++ pp3/pp3.cpp
@@ -1,3 +1,15 @@
+/*
+ * PROGRAM PROJECT 3
+ * 
+ *
+ * Lovodi
+ * Date created: 4/19/14
+ * Last date modified: 4/25/14
+ *
+ * SOURCES USED
+ * 
+ */
+
 #include "DLNode.h"
 #include "Operator.h"
 #include "DLList.h"
@@ -8,11 +20,17 @@
 #include <sstream>
 using namespace std;
 
-int main(int  , char** )
+int main(int argv, char**  argc)
 {
-    string temporary("file.txt");
-    Operator runProgram(temporary);
-    runProgram.run();
-    
+    if(argv==2)
+    {
+        string program(argc[1]);
+        Operator runProgram(program);
+        runProgram.run();
+    }
+    else
+    {
+        cout << "Incorrect code, please input the correct code." << endl;
+    }
     return 0;
 }
